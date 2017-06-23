61ae8f607c0c40181bac7ddafdc09b78eb3b8d4a
Alexandr Dudarkov <alexdudarkov@gmail.com>
2017-06-23 17:17:51 +0300

    firt commit

---
+++ src/main/java/gmail/alexdudarkov/firstmvn/App.java
@@ -0,0 +1,13 @@
+package gmail.alexdudarkov.firstmvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
